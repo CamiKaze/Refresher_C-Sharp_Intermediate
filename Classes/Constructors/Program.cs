0d64c2dda915bdd07fb9515458604696bbd7aefd
Cameron Smith <cameronsmith63@gmail.com>
2020-04-28 12:33:58 +0200

    Added Object Initialiser example.

--- Classes/Constructors/Program.cs
+++ Classes/Constructors/Program.cs
@@ -9,6 +9,18 @@ namespace Constructors
             var customer = new Customer(1, "Cameron");
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
+
+            // Object Initializer is a syntax for quickly
+            // initialising an object without the need
+            // to call one of its constructors.
+            // We do this to avoid creating multiple constructors.
+            var customer1 = new Customer
+            { // Object initialisation syntax is when we assign
+              // the name of properties and assign them some
+              // intial value.
+                Id = 1,
+                Name = "Cameron"
+            };
         }
     }
 }
