6317db1d64282cdfbd56d32ef12ca3e8ae3bebd5
Cameron Smith <cameronsmith63@gmail.com>
2020-04-29 12:40:21 +0200

    Inheritance example

---
+++ Association between Classes/Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            // We now hgave access to the properties
+            // and methods in the parent class.
+            text.Width = 100;
+            text.Copy();
+        }
+    }
+}
