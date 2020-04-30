0f4b999115f49d8df3bd6f8386b20030df51365b
Cameron Smith <cameronsmith63@gmail.com>
2020-04-30 14:46:42 +0200

    Boxing and Unboxing

---
+++ 03 - Inheritance/Boxing and Unboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boxing_and_Unboxing
+{
+    class Program
+    { // Boxing/Unboxing has a performance penalty.
+      // It is better to use a generic list implementation
+      // of the class.
+        static void Main(string[] args)
+        {
+            // The problem witrh an ArrayList is type safety.
+            var list = new ArrayList();
+            // Boxing will happen with this value as an
+            // ArrayList takes an object of type object.
+            list.Add(1);
+            // Boxing with not happen with this reference.
+            list.Add("Cameron");
+            // Boxing will happen with the value below.
+            list.Add(DateTime.Today);
+
+            // The best option is to use a genereic list.
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+        }
+    }
+}
